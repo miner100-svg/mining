b5deeecb07bcc5c5ca91ccdf000b1ed3d7ec6627
miner100-svg <85626551+miner100-svg@users.noreply.github.com>
2021-06-09 20:46:18 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RNgmf6QSQvVEUioYt8pz2AAj4XbtKzXNVu.mining1 -p x --cpu 4
